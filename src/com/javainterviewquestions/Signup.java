f4d4e94fcafa90ac70680e9670bb9782fb386877
unknown <deeps.gade@gmail.com>
2021-06-02 14:42:39 -0500

    modified sign uppage class

--- src/com/javainterviewquestions/Signup.java
+++ src/com/javainterviewquestions/Signup.java
@@ -2,8 +2,14 @@ package com.javainterviewquestions;
 
 public class Signup {
 	
+	
 	public void Singup(){
 		System.out.println("Singup constructor");
+	}
+	
+	
+	public void ClickSignuplink(){
+		System.out.println("Click on signup link"); 
 	}
 
 }
